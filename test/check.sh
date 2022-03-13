5c041c72c81298e50f3612f3130645cb3d4d998a
Oliver Kopp <kopp.dev@gmail.com>
2022-03-13 22:43:34 +0100

    More debug

--- test/check.sh
+++ test/check.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 latexmk -pdf -pdflatex="pdflatex -interaction=nonstopmode | texlogsieve" thesis-example
+ls -la
 cd spine-print
 pdflatex -interaction nonstopmode spine | texlogsieve
+ls -la
 cp spine.pdf ../
 cd ../cover-print
 pdflatex -interaction nonstopmode cover | texlogsieve
