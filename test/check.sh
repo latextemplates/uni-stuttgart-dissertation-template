29a310e26dcb8d8f24e46d1682c77758cce11c54
Oliver Kopp <kopp.dev@gmail.com>
2022-03-13 23:19:31 +0100

    Try exit 1

--- test/check.sh
+++ test/check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-latexmk -pdf -pdflatex="pdflatex --interaction=nonstopmode" thesis-example
+latexmk -pdf -pdflatex="pdflatex --interaction=nonstopmode" thesis-example || exit 1
 ls -la
 cd spine-print
-pdflatex --interaction=nonstopmode spine
+pdflatex --interaction=nonstopmode spine || exit 1
 ls -la
 cp spine.pdf ../
 cd ../cover-print
-pdflatex --interaction=nonstopmode cover
+pdflatex --interaction=nonstopmode cover || exit 1
 cp cover.pdf ../
